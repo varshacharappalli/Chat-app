df1df48a20afc9f695e440d9194c5230ef232511
varshacharappalli <167790510+varshacharappalli@users.noreply.github.com>
2025-03-03 21:20:58 +0530

    Project completed

--- frontend/src/store/userAuthStore.js
+++ frontend/src/store/userAuthStore.js
@@ -4,7 +4,7 @@ import toast from 'react-hot-toast';
 import axios from "axios";
 import {io} from 'socket.io-client';
 
-const BASE_URL=import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+const BASE_URL=import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/";
 
 
 export const userAuthStore=create((set,get)=>({
